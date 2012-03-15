df0d2ff058147116a1ebb7711166f410e612beb2
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2012-03-16 02:38:54 +0900

    Fixed various styles and fields of model.
    Stop to use camelCase.
    Fixed some mistakes.

--- models.py
+++ models.py
@@ -1,10 +1,30 @@
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 MAX_KILO_DIGITS = 6
+MAX_ENGLISH_NAME = 128
+MAX_LOCALIZED_NAME = 256
+MAX_LINE_NAME_LENGTH = 128
+MAX_LINE_NICKNAME_LENGTH = 128
 
 
-class Country(models.Model):
+class EnglishAndLocalizedNameObject(models.Model):
+
+    """Abstract class for object with English and localized name."""
+
+    english_name = models.CharField(_("English name of the %(class)s"),
+                                    max_length=MAX_ENGLISH_NAME)
+    local_name = models.CharField(_("localized name of the %(class)s"),
+                                  max_length=MAX_LOCALIZED_NAME)
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        return self.local_name
+
+
+class Country(EnglishAndLocalizedNameObject):
 
     """Country"""
 
@@ -16,34 +36,22 @@ class Country(models.Model):
     code3 = models.SlugField(_("ISO 3166-1 alpha-3"),
                              max_length=3,
                              unique=True)
-    englishName = models.CharField(_("country name in English"),
-                                   max_length=128)
-    localName = models.CharField(_("country name in local language"),
-                                 max_length=256)
 
 
-class CountryArea(models.Model):
+class DomesticRegion(EnglishAndLocalizedNameObject):
 
-    """Wide area in the country"""
+    """wider area in the country"""
 
     country = models.ForeignKey(Country)
-    englishName = models.CharField(_("name of the area in English"),
-                                   max_length=128)
-    localName = models.CharField(_("name of the area in local language"),
-                                 max_length=128)
 
 
-class District(models.Model):
+class District(EnglishAndLocalizedNameObject):
 
     """Smaller area in the country"""
 
-    countryArea = models.ForeignKey(CountryArea)
+    domesticregion = models.ForeignKey(DomesticRegion)
     code = models.SlugField(_("ISO 3166-2"),
                             max_length=6)
-    englishName = models.CharField(_("name of the district in English"),
-                                   max_length=128)
-    localName = models.CharField(_("name of the district in local language"),
-                                 max_length=128)
 
 
 class CompanyType(models.Model):
@@ -51,16 +59,36 @@ class CompanyType(models.Model):
     """The type of the railway company"""
 
     description = models.CharField(_("type of the railway"),
-                                   max_length=64)
+                                   max_length=64,
+                                   unique=True)
+
+    def __unicode__(self):
+        return self.description
 
 
 class Company(models.Model):
 
     """Railway company"""
 
-    companyType = models.ForeignKey(CompanyType)
+    companytype = models.ForeignKey(CompanyType)
     name = models.CharField(_("name of the company"),
                             max_length=256)
+
+    def __unicode__(self):
+        return self.name
+
+
+LINE_TYPE_CHOICES = (
+    (1, _("high speed train")),
+    (2, _("normal train")),
+    (3, _("tram train")),
+    (4, _("suspended monorail")),
+    (5, _("straddle monorail")),
+    (6, _("guideway transit")),
+    (7, _("funicular")),
+    (8, _("trolleybus")),
+    (9, _("Maglev train")),
+    )
 
 
 class Line(models.Model):
@@ -75,9 +103,19 @@ class Line(models.Model):
     sort = models.IntegerField(_("sort key of line in ekidata"),
                                blank=True,
                                null=True)
+    line_type = models.SmallIntegerField(_("technical line type"),
+                                         choices=LINE_TYPE_CHOICES)
     name = models.CharField(_("name of the line"),
-                            max_length=256)
-    enabled = models.BooleanField(_("enabled"))
+                            max_length=MAX_LINE_NAME_LENGTH)
+    nickname = models.CharField(_("nickname of the line"),
+                            max_length=MAX_LINE_NICKNAME_LENGTH)
+    enabled = models.BooleanField(_("enabled flag"))
+    defunct_date = models.DateField(_("date when get defunct if disabled"),
+                                    blank=True,
+                                    null=True)
+
+    def __unicode__(self):
+        return unicode(self.company) + u',' + unicode(self.name)
 
 
 class Station(models.Model):
@@ -92,9 +130,9 @@ class Station(models.Model):
     sort = models.IntegerField(_("sort key of station in ekidata"),
                                blank=True,
                                null=True)
-    groupCode = models.IntegerField(_("group code of the station in ekidata"),
-                                    blank=True,
-                                    null=True)
+    group_code = models.IntegerField(_("group code of the station in ekidata"),
+                                     blank=True,
+                                     null=True)
     name = models.CharField(_("name of the station"),
                             max_length=256)
     kilo = models.DecimalField(_("kilo from the origination"),
@@ -103,24 +141,35 @@ class Station(models.Model):
                                blank=True,
                                null=True)
     district = models.ForeignKey(District)
-    isUnderground = models.BooleanField(_("subway station flag"))
+    is_underground = models.BooleanField(_("subway station flag"))
     longitude = models.DecimalField(_("longitude of the station"),
                                     max_digits=9,
                                     decimal_places=6)
     latitude = models.DecimalField(_("latitude of the station"),
                                    max_digits=9,
                                    decimal_places=6)
-    enabled = models.BooleanField(_("enabled"))
+    enabled = models.BooleanField(_("enabled flag"))
+
+    def __unicode__(self):
+        return unicode(self.line) + u',' + unicode(self.name)
 
 
 class AdjacentStation(models.Model):
 
     """Adjacence of the station"""
 
-    station1 = models.ForeignKey(Station, related_name="set1")
-    station2 = models.ForeignKey(Station, related_name="set2")
+    line = models.ForeignKey(Line)
+    station1 = models.ForeignKey(Station, related_name="adjacent_station1")
+    station2 = models.ForeignKey(Station, related_name="adjacent_station2")
     kilo = models.DecimalField(_("kilo between the stations"),
                                max_digits=MAX_KILO_DIGITS,
-                               decimal_places=1)
-    interPoints = models.CharField(_("intermediate points of the adjacence"),
-                                   max_length=2048)
+                               decimal_places=1,
+                               blank=True,
+                               null=True)
+    inter_points = models.CharField(_("intermediate points of the adjacence"),
+                                    max_length=2048,
+                                    blank=True)
+
+    def __unicode__(self):
+        text = (self.line, self.station1.name, self.station2.name)
+        return u','.join(unicode(i) for i in text)
