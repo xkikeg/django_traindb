15b334228615c5c37c05b155609c90b54b4a4581
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2012-03-16 02:41:16 +0900

    Implemented CSV import script.
    Yet a bug remains. In finding related object, this script uses a lambda.
    In this lambda, referring model object is updated with latter one.
    For example, foo field refers Foo.id and bar field refers Bar.id.
    In this situation, this script get Bar object even for foo mistakely.

--- management/commands/einhorn_importcsv.py
+++ management/commands/einhorn_importcsv.py
@@ -1,29 +1,146 @@
 #!/usr/bin/python
 
-import datetime
+import os
+import re
 import csv
+import sys
 from optparse import make_option
+from ConfigParser import SafeConfigParser
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
-from comic.models import Series, Category, DailyEntry
+from django.db import models
+import django_traindb.models
+
+DEBUG = True
+# DEBUG = False
 
 
-def csv_into_entry(datafile, is_dry=True, **kwargs):
-    f = csv.reader(open(datafile), **kwargs)
-    header = f.next()
-    column = {}
-    for i, x in enumerate(header):
-        column[x.lstrip('#').rstrip('*[]')] = i
-    for row in f:
-        if len(row) == 0 or len(row[0]) == 0 or row[0][0] == '#': continue
-        series = Series.objects.get(code=row[column["series"]])
-        date = datetime.date(*(int(i) for i in row[column["date"]].split("-")))
+"""
+Generic CSV file importer.
+Give the config file or guess field name from CSV header.
+"""
+
+
+def get_model_class(model_name_string):
+    return getattr(globals()['django_traindb'].models,
+                   model_name_string)
+
+
+class CSVConfigManager(object):
+
+    """CSV loading configuration manager which can import csv data."""
+
+    def __init__(self, configfile, model=None):
+        if configfile is None:
+            assert model is not None
+            self.configparser = None
+            if isinstance(model, models.Model):
+                self.model = model
+            else:
+                self.model = get_model_class(model)
+        else:
+            if not os.path.isfile(configfile):
+                raise IOError('config file not found: %s' % configfile)
+            parser = SafeConfigParser()
+            parser.read(configfile)
+            self.modelstring = parser.sections()[0]
+            self.model = get_model_class(self.modelstring)
+            self.configparser = parser
+
+    def set_header(self, header):
+        self.column = [x for x in header]
+        self.field = [None] * len(self.column)
+        self.conv = [None] * len(self.column)
+        self.referred = {}
+        for i, label in enumerate(self.column):
+            if (self.configparser is None
+                or not self.configparser.has_option(self.modelstring, label)):
+                self.field[i] = label
+                self.conv[i] = lambda x: x
+                continue
+            setting = self.configparser.get(self.modelstring, label).split('=', 1)
+            M = re.compile(r'([^[]+)(?:\[(.+?)\])?').match(setting[0])
+            assert M, "type of label '%(field)s' is invalid: %(setting)s" % {
+                    "field": label,
+                    "setting": setting[0],
+                    }
+            command, target = M.groups()
+            if command == "ignore":
+                if DEBUG: print "%d '%s': ignore" % (i, label)
+                self.field[i] = None
+                self.conv[i] = lambda x: None
+            elif command == "field":
+                if DEBUG: print "%d '%s': field[%s]" % (i, label, target),
+                self.field[i] = target
+                if len(setting) == 1:
+                    if DEBUG: print "pass"
+                    self.conv[i] = lambda x: x
+                    continue
+                N = re.compile(r'([^(]+)(?:\((.+?)\))').match(setting[1])
+                assert N, "option of label '%(field)s' is invalid: %(setting)s" % {
+                    "field": label,
+                    "setting": setting[1],
+                    }
+                convcommand, convarg = N.groups()
+                if convcommand == "refer":
+                    if DEBUG: print "refer", convarg
+                    self.referred[label] = convarg
+                    ref_name, ref_field = convarg.split('.')
+                    ref_model = get_model_class(ref_name)
+                    # lookup convarg with x
+                    self.conv[i] = (
+                        lambda x: ref_model.objects.get(**{ref_field: x}))
+                elif convcommand == "eval":
+                    if DEBUG: print "eval", convarg
+                    self.conv[i] = eval(convarg)
+                else:
+                    assert False, "incorrect: " + setting[1]
+            else:
+                assert False, "incorrect: " + setting[0]
+        pass
+
+    def get_field_name(self, column):
+        return self.field[column]
+
+    def get_field_value(self, column, string):
         try:
-            cats = [Category.objects.get(code=i.strip()) for i in row[column["category"]].split(",")]
+            return self.conv[column](string)
         except ObjectDoesNotExist:
-            print row[column["category"]].split(",")
+            print >>sys.stderr, column, self.referred[self.column[column]], string
             raise
-        print date, cats
+
+    def make_model_args(self, row):
+        ret = {}
+        for i, x in enumerate(row):
+            fn = self.get_field_name(i)
+            if fn is None: continue
+            ret[fn] = self.get_field_value(i, x.decode("UTF-8"))
+        return ret
+
+    def import_csv(self, datafile, is_dry=True, **kwargs):
+        f = csv.reader(open(datafile), **kwargs)
+        header = f.next()
+        self.set_header(header)
+        for row in f:
+            if len(row) == 0 or (len(row[0]) != 0 and row[0][0] == '#'):
+                continue
+            try:
+                make_model_kwargs = self.make_model_args(row)
+            except ObjectDoesNotExist:
+                print >>sys.stderr, row
+                raise
+            if self.model.objects.filter(**make_model_kwargs).count() == 0:
+                if DEBUG: print "create:", make_model_kwargs
+                obj = self.model(**make_model_kwargs)
+                if not is_dry:
+                    obj.save()
+            else:
+                if DEBUG: print "ignore:", make_model_kwargs
+        return
+
+
+def csv_into_entry():
+        cats = [Category.objects.get(code=i.strip()) for i in row[column["category"]].split(",")]
         d = DailyEntry(series=series,
                        date=date,
                        title_ja=row[column["title_ja"]],
@@ -34,9 +151,10 @@ def csv_into_entry(datafile, is_dry=True, **kwargs):
             d.save()
             d.categories = cats
 
+
 class Command(BaseCommand):
-    help = 'Import data from old canal format to daily comic entry'
-    args = "csvfile [csvfile..]"
+    help = 'Import data from traindb'
+    args = "csvfile"
     option_list = BaseCommand.option_list + (
         make_option('--delimiter', action='store', dest='delimiter',
                     default=',', help="CSV delimiter character."),
@@ -44,12 +162,20 @@ class Command(BaseCommand):
                     default='"', help="CSV quotation character."),
         make_option('-n', '--dry-run', action='store_true', dest='dry',
                     default=False, help="Only print data without importing."),
+        make_option('--config', action='store', dest='config',
+                    default=None, help="Set config file."),
+        make_option('--model', action='store', dest='model',
+                    default=None, help="Set target model without config."),
         )
 
     def handle(self, *csv_labels, **option):
         is_dry = option.get('dry')
         delimiter = option.get('delimiter')
         quotechar = option.get('quotechar')
+        config = option.get('config')
+        model = option.get('model')
+        manager = CSVConfigManager(config, model)
         for i in csv_labels:
             print i
-            csv_into_entry(i, delimiter=delimiter, quotechar=quotechar, is_dry=is_dry)
+            manager.import_csv(i, is_dry=is_dry,
+                               delimiter=delimiter, quotechar=quotechar)
